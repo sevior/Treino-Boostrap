1c1e597f3fd18cba3afcd6aed186ca615fdf8b2e
Gicélio da Silva Júnior <gicelio.silva@outlook.com>
2020-05-14 23:50:54 -0300

    Atualização das cores do site

---
+++ script.js
@@ -0,0 +1,8 @@
+function proximo() {
+    document.getElementById('form1').style.visibility = 'hidden';
+    document.getElementById('form2').style.visibility = 'visible';
+}
+function voltar() {
+    document.getElementById('form1').style.visibility = 'visible';
+    document.getElementById('form2').style.visibility = 'hiiden';
+}
